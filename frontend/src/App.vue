282440f9cea50668efc149f6b75fceb422e39d00
okawa <0cu2045d@gmail.com>
2023-12-28 00:40:20 +0900

    PandaCssインストール

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { css } from "../styled-system/css";
+
 </script>
 
 <template>
+	<div :class="css({ fontSize: '5xl', fontWeight: 'bold', color:'red' })">Hello 🐼!</div>
 	<RouterView />
 </template>
