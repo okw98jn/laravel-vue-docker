08aae110dc9c58dd6a42af239b022eb77fac40fb
okawa <0cu2045d@gmail.com>
2024-01-03 22:28:33 +0900

    tailwindインストール

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,11 @@
-import './index.css'
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createPinia } from 'pinia'
 import router from './router'
+import './index.css'
 
 const app = createApp(App)
 
+app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
