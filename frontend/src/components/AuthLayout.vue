06ed0ffc8252dd474e0af8b32d7548e6124b4a46
okawa <0cu2045d@gmail.com>
2024-01-04 01:08:14 +0900

    画面作成

---
+++ frontend/src/components/AuthLayout.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div class="max-w-md w-full space-y-8">
+            <RouterView />
+        </div>
+    </div>
+</template>
