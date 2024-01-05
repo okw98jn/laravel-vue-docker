ef2468fcc6c7e2eb65f95a8ecdac58d644c906c4
okawa <0cu2045d@gmail.com>
2024-01-06 02:08:28 +0900

    propsを分割代入で取得

--- frontend/src/components/PageLayout.vue
+++ frontend/src/components/PageLayout.vue
@@ -2,13 +2,13 @@
 type Props = {
     title: string
 }
-const props = defineProps<Props>()
+const { title } = defineProps<Props>()
 </script>
 
 <template>
     <header class="bg-white shadow">
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ props.title }}</h1>
+            <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ title }}</h1>
         </div>
     </header>
     <main>
