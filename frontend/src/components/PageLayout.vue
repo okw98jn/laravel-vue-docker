cff232f2f9fc40d65b504b281043df404a373d20
okawa <0cu2045d@gmail.com>
2024-01-07 16:19:44 +0900

    ヘッダーにslot作成

--- frontend/src/components/PageLayout.vue
+++ frontend/src/components/PageLayout.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 type Props = {
-    title: string
+    title?: string
 }
 const { title } = defineProps<Props>()
 </script>
@@ -9,6 +9,7 @@ const { title } = defineProps<Props>()
     <header class="bg-white shadow">
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
             <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ title }}</h1>
+            <slot name="header"></slot>
         </div>
     </header>
     <main>
