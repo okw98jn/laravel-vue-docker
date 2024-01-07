47b12eab20b51eb34674305c6e8a82386120e2b3
okawa <0cu2045d@gmail.com>
2024-01-07 18:43:58 +0900

    リポジトリのバインド記述リファクタリング

--- backend/app/Providers/RepositoryServiceProvider.php
+++ backend/app/Providers/RepositoryServiceProvider.php
@@ -6,11 +6,40 @@ use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
 {
+    /**
+     * サービスの登録
+     *
+     * @return void
+     */
     public function register()
+    {
+        $this->bindUserRepository();
+        $this->bindSurveyRepository();
+    }
+
+    /**
+     * Userリポジトリインターフェースとその実装をバインドします
+     *
+     * @return void
+     */
+    protected function bindUserRepository()
     {
         $this->app->bind(
             \App\Repositories\User\UserRepositoryInterface::class,
             \App\Repositories\User\UserRepository::class
+        );
+    }
+
+    /**
+     * Surveyリポジトリインターフェースとその実装をバインドします
+     *
+     * @return void
+     */
+    protected function bindSurveyRepository()
+    {
+        $this->app->bind(
+            \App\Repositories\Survey\SurveyRepositoryInterface::class,
+            \App\Repositories\Survey\SurveyRepository::class
         );
     }
 }
