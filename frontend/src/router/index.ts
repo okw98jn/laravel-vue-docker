824d513b40363810dcdcb33c290ee136e5962e0b
okawa <0cu2045d@gmail.com>
2024-01-07 16:18:35 +0900

    アンケート登録、編集のルーティング作成

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -6,6 +6,7 @@ import Surveys from '@/views/Surveys.vue'
 import AuthLayout from '@/components/auth/AuthLayout.vue'
 import Login from '@/views/Login.vue'
 import Register from '@/views/Register.vue'
+import SurveyView from '@/views/SurveyView.vue'
 
 const routes = [
 	{
@@ -16,6 +17,8 @@ const routes = [
 		children: [
 			{ path: '/dashboard', name: 'Dashboard', component: Dashboard },
 			{ path: '/surveys', name: 'Surveys', component: Surveys },
+			{ path: '/surveys/create', name: 'SurveyCreate', component: SurveyView },
+			{ path: '/surveys/:id', name: 'SurveyView', component: SurveyView },
 		]
 	},
 	{
