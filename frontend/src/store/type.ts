919ed800a73f4f1249ad8ac5d5b66fd1822537ba
okawa <0cu2045d@gmail.com>
2024-01-09 01:09:38 +0900

    storeの型を別ファイルに移動

---
+++ frontend/src/store/type.ts
@@ -0,0 +1,20 @@
+import type { Survey } from "@/types/Survey";
+import type { User } from "@/types/User";
+
+export type UserState = {
+    user: {
+        data: User;
+        token: string | null;
+    };
+};
+
+export type CurrentSurveyState = {
+    currentSurvey: {
+        loading: boolean;
+        data: Survey;
+    }
+};
+
+export type SurveyState = {
+    surveys: Survey[];
+};
